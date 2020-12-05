b0d6b68c6d8a49c12474e3a79bd6dd210df3d2a8
Damy Metzke <damy.metzke@gmail.com>
2020-12-05 16:25:14 +0100

    Refactor routeparameter from route template.

--- src/writer/htmlWriter.ts
+++ src/writer/htmlWriter.ts
@@ -6,7 +6,11 @@ import { RouteData } from '../data/routeData';
 
 export default class HtmlWriter implements IWriter {
   async writeRoute(data: RouteData): Promise<string> {
-    const template = handlebars.compile(await (await fs.readFile(path.join(__dirname, '../../resource/htmlTemplate/route.handlebars'))).toString());
+    handlebars.registerPartial('routeParameter', handlebars.compile(
+      (await fs.readFile(path.join(__dirname, '../../resource/htmlTemplate/routeParameter.handlebars'))).toString(),
+    ));
+
+    const template = handlebars.compile((await fs.readFile(path.join(__dirname, '../../resource/htmlTemplate/route.handlebars'))).toString());
     return template(data);
   }
 }
