2b348826015363e6f0ec35f248cc7b946ae79691
Damy Metzke <damy.metzke@gmail.com>
2020-12-09 10:46:33 +0100

    Add GroupData interface.

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 export { default as AllData } from './allData';
 export { default as GlobalData } from './globalData';
+export { default as GroupData } from './groupData';
 export { default as MethodData } from './methodData';
 export { default as ParameterData } from './parameterData';
 export { default as RouteData } from './routeData';
