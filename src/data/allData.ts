3286e65c89713b00adbe53fe55435e8a3b522668
Damy Metzke <damy.metzke@gmail.com>
2020-12-09 11:33:42 +0100

    Parse group data.

--- src/data/allData.ts
+++ src/data/allData.ts
@@ -1,8 +1,10 @@
 import RouteData from './routeData';
 import GlobalData from './globalData';
+import GroupData from './groupData';
 
 export default interface AllData
 {
     routes: RouteData[];
+    groups: GroupData[];
     global: GlobalData;
 }
