f1a9df33480491beb37ac61ffc5e512f23f0de17
Damy Metzke <damy.metzke@gmail.com>
2020-12-14 10:19:07 +0100

    Add ValidateResult.

--- test/validate/validator.test.ts
+++ test/validate/validator.test.ts
@@ -1,17 +1,36 @@
 import Validator from "../../src/validate/validator";
+import ValidateResult from "../../src/validate/validateResult";
 
 test("Validator will return true with valid data", () => {
   const validator = new Validator();
 
-  expect(validator.validate({ a: "Hello World!" })).toStrictEqual(true);
+  expect(validator.validate({ a: "Hello World!" })).toStrictEqual({
+    success: true,
+    data: { a: "Hello World!" },
+  });
 });
 
 test("Validator will return false with non-object and null input", () => {
   const validator = new Validator();
 
-  expect(validator.validate(null)).toStrictEqual(false);
-  expect(validator.validate(42)).toStrictEqual(false);
-  expect(validator.validate(false)).toStrictEqual(false);
-  expect(validator.validate("Hello World!")).toStrictEqual(false);
-  expect(validator.validate(() => {})).toStrictEqual(false);
+  expect(validator.validate(null)).toStrictEqual({
+    success: false,
+    data: null,
+  });
+  expect(validator.validate(42)).toStrictEqual({
+    success: false,
+    data: null,
+  });
+  expect(validator.validate(false)).toStrictEqual({
+    success: false,
+    data: null,
+  });
+  expect(validator.validate("Hello World!")).toStrictEqual({
+    success: false,
+    data: null,
+  });
+  expect(validator.validate(() => {})).toStrictEqual({
+    success: false,
+    data: null,
+  });
 });
