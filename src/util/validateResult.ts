f0ae0de70f7e64b3707a71af8153c0f38871b24e
Damy Metzke <damy.metzke@gmail.com>
2020-12-21 11:18:08 +0100

    Add utility functions for adding to objects.

--- src/util/validateResult.ts
+++ src/util/validateResult.ts
@@ -1,5 +1,23 @@
 import ValidateResult from "../validate/validateResult";
 
+function deepMergeValidateResultArray(
+  total: ValidateResult,
+  current: ValidateResult,
+  objectName: string
+): ValidateResult {
+  return {
+    success: total.success && current.success,
+    data: { [objectName]: [...total.data[objectName], current.data[""]] },
+    extra: [...total.extra, ...current.extra],
+    failed: [...total.failed, ...current.failed],
+    missing: [...total.missing, ...current.missing],
+    messages: [...total.messages, ...current.messages],
+  };
+}
+
+/**
+ * @deprecated use addToObject or addArrayToObject instead
+ */
 export function addKeyPrefixToValidateResult(
   a: ValidateResult,
   prefix: string
@@ -12,6 +30,23 @@ export function addKeyPrefixToValidateResult(
     missing: a.missing.map((key) => `${prefix}${key}`),
     messages: a.messages.map(({ key, problem }) => ({
       key: `${prefix}${key}`,
+      problem,
+    })),
+  };
+}
+
+export function addToObject(
+  a: ValidateResult,
+  objectName: string
+): ValidateResult {
+  return {
+    success: a.success,
+    data: { [objectName]: a.data },
+    extra: a.extra.map((key) => `${objectName}.${key}`),
+    failed: a.failed.map((key) => `${objectName}.${key}`),
+    missing: a.missing.map((key) => `${objectName}.${key}`),
+    messages: a.messages.map(({ key, problem }) => ({
+      key: `${objectName}.${key}`,
       problem,
     })),
   };
@@ -53,4 +88,27 @@ export function succesfulDataValidateResult(data: {
     missing: [],
     messages: [],
   };
+}
+
+export function addArrayToObject(
+  a: ValidateResult[],
+  objectName: string
+): ValidateResult {
+  return a
+    .map((value, index) => ({
+      success: value.success,
+      data: { "": value.data },
+      extra: value.extra.map((key) => `[${index}].${key}`),
+      failed: value.failed.map((key) => `${index}.${key}`),
+      missing: value.missing.map((key) => `${index}.${key}`),
+      messages: value.messages.map(({ key, problem }) => ({
+        key: `[${index}].${key}`,
+        problem,
+      })),
+    }))
+    .reduce(
+      (total, current) =>
+        deepMergeValidateResultArray(total, current, objectName),
+      succesfulDataValidateResult({ [objectName]: [] })
+    );
 }
